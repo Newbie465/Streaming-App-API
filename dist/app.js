2c2600e99e18a34817f68c57b52d552e6699a454
Newbie465 <chiragbhatnagar465@gmail.com>
2023-08-05 01:04:44 +0530

    Added Google Auth and fixed .env issue

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,10 @@ const fastify_1 = __importDefault(require("fastify"));
 const searchController_1 = __importDefault(require("./controllers/searchController"));
 const swagger_1 = require("@fastify/swagger");
 const swagger_ui_1 = __importDefault(require("@fastify/swagger-ui"));
-const envConnector_1 = __importDefault(require("./utils/envConnector"));
+const cors_1 = require("@fastify/cors");
+const oAuthMiddleware_1 = __importDefault(require("./utils/oAuthMiddleware"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.configDotenv)();
 class Application {
     constructor() {
         this.fastify = (0, fastify_1.default)({
@@ -24,6 +27,7 @@ class Application {
                 },
                 tags: [
                     { name: 'Search', description: 'All Search EndPoints' },
+                    { name: 'Authentication', description: 'All Authentication EndPoints' }
                 ]
             }
         });
@@ -31,13 +35,13 @@ class Application {
             routePrefix: "/swagger/docs"
         });
         this.fastify.register(searchController_1.default, { prefix: "/api/v1" });
-        this.fastify.register(envConnector_1.default).ready(err => {
-            if (err)
-                console.log(err);
-            this.fastify.listen({
-                port: Number(this.fastify.config.PORT) || 8080,
-                host: this.fastify.config.HOST || "localhost",
-            });
+        this.fastify.register(cors_1.fastifyCors, {
+            origin: "*",
+        });
+        this.fastify.register(oAuthMiddleware_1.default);
+        this.fastify.listen({
+            port: Number(process.env.PORT) || 3000,
+            host: process.env.HOST || "localhost",
         });
     }
 }
