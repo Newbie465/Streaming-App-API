10f2c507bd1de4bc5bd974dd770c8fc881cf71bd
Newbie465 <chiragbhatnagar465@gmail.com>
2023-08-13 00:26:52 +0530

    Added Neon Postgres Db

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,32 @@ import fastifySwaggerUi from "@fastify/swagger-ui";
 import { fastifyCors } from "@fastify/cors";
 import oAuthMiddleware from "./utils/oAuthMiddleware";
 import { configDotenv } from "dotenv";
+import {migrate} from "drizzle-orm/node-postgres/migrator"
+import {logger} from "./utils/logger"
+import { db } from "./db";
+import { env } from "process";
+import { usersRoutes } from "./modules/users/user.route";
+import { fastifyJwt } from "@fastify/jwt";
 
 configDotenv();
 
+
+declare module "@fastify/jwt" {
+    interface FastifyJWT {
+      payload: { 
+        id: string, 
+        email: string 
+        }
+      user: {
+            id : string,
+            email: string,
+        }
+    }
+}
 export default class Application {
 
     fastify = fastify({
-        logger: true
+        logger,
     })
 
     constructor() {
@@ -20,7 +39,7 @@ export default class Application {
             
     }
     
-    initialize() {
+    async initialize() {
 
         this.fastify.register(fastifySwagger, {
             swagger: {
@@ -30,13 +49,18 @@ export default class Application {
                 },
                 tags: [
                     { name: 'Search', description: 'All Search EndPoints' },
-                    { name : 'Authentication', description: 'All Authentication EndPoints'}
+                    { name : 'Authentication', description: 'All Authentication EndPoints'},
+                    { name : 'Users', description: "All Users EndPoints"}
                 ]
             }
         });
 
         this.fastify.register(fastifySwaggerUi, {
             routePrefix : "/swagger/docs"
+        })
+
+        this.fastify.register(fastifyJwt, {
+            secret : process.env.JWTSECRET as string,
         })
 
         this.fastify.get("/", async (req : FastifyRequest, rep : FastifyReply) => {
@@ -46,6 +70,7 @@ export default class Application {
         })
     
         this.fastify.register(search, {prefix: "/api/v1/search"})
+        this.fastify.register(usersRoutes, {prefix: "api/v1/users"})
 
         this.fastify.register(fastifyCors, {
             origin: "*",
@@ -53,9 +78,13 @@ export default class Application {
         
         this.fastify.register(oAuthMiddleware)
         
-        this.fastify.listen({
+        await this.fastify.listen({
             port: Number(process.env.PORT) || 3000,
             host: process.env.HOST || "localhost",
+        })
+
+        await migrate(db, {
+            migrationsFolder : "./migrations-folder"
         })
             
     }
